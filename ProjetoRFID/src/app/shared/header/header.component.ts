80c78d3f0f56f7f843434679e706ff120708e772
Victor Quirino <victorsouzaq@gmail.com>
2024-08-02 15:05:50 -0300

    initial header and side menu structure

--- ProjetoRFID/src/app/shared/header/header.component.ts
+++ ProjetoRFID/src/app/shared/header/header.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { ToolbarModule } from 'primeng/toolbar';
+import { ButtonModule } from 'primeng/button';
+import { SidebarComponent } from '../sidebar/sidebar.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [
+    ToolbarModule,
+    ButtonModule,
+    SidebarComponent,
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  sidebarVisible: boolean = false;
 
+  toggleSidebar(): void {
+    this.sidebarVisible =! this.sidebarVisible;
+  }
 }
