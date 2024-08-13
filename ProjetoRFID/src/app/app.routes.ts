b1f12695b6e1fb37f691457d0f7e2868a501a2a7
Victor Quirino <victorsouzaq@gmail.com>
2024-08-13 15:08:51 -0300

    [#28]-Product list page

--- ProjetoRFID/src/app/app.routes.ts
+++ ProjetoRFID/src/app/app.routes.ts
@@ -1,6 +1,8 @@
+import { ProductListComponent } from './pages/product-list/product-list.component';
 import { ProductRegisterComponent } from './pages/product-register/product-register.component';
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    { path: 'produtos/cadastrar', component: ProductRegisterComponent }
+    { path: 'produtos/cadastrar', component: ProductRegisterComponent },
+    { path: 'produtos', component: ProductListComponent},
 ];
