ec9076141dce4382196ddbe80ae727d581d87e73
Victor Quirino <victorsouzaq@gmail.com>
2024-09-18 15:18:54 -0300

    [#66]-Tornando a sidebar fixa

--- ProjetoRFID/src/app/shared/sidebar/sidebar.component.ts
+++ ProjetoRFID/src/app/shared/sidebar/sidebar.component.ts
@@ -3,6 +3,7 @@ import { SidebarModule } from 'primeng/sidebar';
 import { ButtonModule } from 'primeng/button';
 import { PanelMenuModule } from 'primeng/panelmenu';
 import { MenuItem } from 'primeng/api';
+import { CardModule } from 'primeng/card';
 
 @Component({
   selector: 'app-sidebar',
@@ -11,12 +12,13 @@ import { MenuItem } from 'primeng/api';
     SidebarModule,
     ButtonModule,
     PanelMenuModule,
+    CardModule,
   ],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
 export class SidebarComponent {
-  @Input() sidebarVisible!: boolean;
+  @Input() sidebarVisible: boolean = true;
   @Output() hideSidebarEvent = new EventEmitter<boolean>();
 
   items: MenuItem[] = [
@@ -24,26 +26,20 @@ export class SidebarComponent {
       label: 'Embalagens',
       icon: 'pi pi-inbox',
       routerLink: ['/embalagens/cadastrar'],
-      command: () => { this.hideSidebar() }
     },
     {
       label: 'Produtos',
       icon: 'pi pi-box',
       routerLink: ['/produtos'],
-      command: () => { this.hideSidebar() } },
+    },
     {
       label: 'Leituras',
       icon: 'pi pi-barcode',
       items: [
-        { label: 'Nova Leitura', icon: 'pi pi-plus', routerLink: ['/leituras/realizar'], command: () => { this.hideSidebar() } },
-        { label: 'Histórico', icon: 'pi pi-history', routerLink: ['/leituras/historico'], command: () => { this.hideSidebar() } },
+        { label: 'Nova Leitura', icon: 'pi pi-plus', routerLink: ['/leituras/realizar'] },
+        { label: 'Histórico', icon: 'pi pi-history', routerLink: ['/leituras/historico'] },
       ]
     },
     
   ];
-
-  hideSidebar(): void {
-    this.sidebarVisible = false;
-    this.hideSidebarEvent.emit(this.sidebarVisible);
-  }
 }
